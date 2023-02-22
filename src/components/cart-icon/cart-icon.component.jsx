61671fc85a46b8e369c745d0c16e2734dd35e3f0
Justin Gabriel <gabriel.justin78@gmail.com>
2023-02-22 14:37:09 +0530

    converted entire application over to styled-components

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,18 +1,21 @@
-import { useContext } from "react";
-import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
+import { useContext } from 'react';
 
-import { CartContext } from "../../contexts/cart.context";
+import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
-import "./cart-icon.styles.scss";
+import { CartContext } from '../../contexts/cart.context';
+
+import { CartIconContainer, ItemCount } from './cart-icon.styles';
 
 const CartIcon = () => {
-    const {isCartOpen, setIsCartOpen,cartCount,} = useContext(CartContext);
-    const ToggleIsCartOpen = () => setIsCartOpen(!isCartOpen)
+  const { isCartOpen, setIsCartOpen, cartCount } = useContext(CartContext);
+
+  const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
+
   return (
-    <div className="cart-icon-container" onClick={ ToggleIsCartOpen }>
-      <ShoppingIcon className="shopping-icon" />
-      <span className="item-count">{cartCount}</span>
-    </div>
+    <CartIconContainer onClick={toggleIsCartOpen}>
+      <ShoppingIcon className='shopping-icon' />
+      <ItemCount>{cartCount}</ItemCount>
+    </CartIconContainer>
   );
 };
 
--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -1,13 +1,14 @@
-import SignUpForm from "../../components/sign-up-form/sign-up-form.components";
-import SignInForm from "../../components/sign-in-form/sign-in-form.components";
-import './authentication.styles.scss'
-const Authentication = () => {
+import SignUpForm from '../../components/sign-up-form/sign-up-form.component'
+import SignInForm from '../../components/sign-in-form/sign-in-form.component';
 
+import { AuthenticationContainer } from './authentication.styles';
+
+const Authentication = () => {
   return (
-    <div className="authentication-container">
+    <AuthenticationContainer>
       <SignInForm />
       <SignUpForm />
-    </div>
+    </AuthenticationContainer>
   );
 };
 
